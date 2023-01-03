3db2c5d51f80eeeaf0c5dd84e16c108dfc332cb6
mccamara <camaramamady9160@gmail.com>
2023-01-03 17:22:48 +0000

    my second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChallengeComponent } from './challenge/challenge.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  // { path: '*', component: NotFoundComponent  },
-  // {
-  //   path: '**',
-  //   redirectTo: 'connexion', pathMatch: 'full'
-  // },
+  {
+    path: '',
+    redirectTo: 'connexion', pathMatch: 'full'
+  },
   { path: 'connexion', component: ConnexionComponent },
   { path: 'dashboard', component: DashboardComponent },
+  {path:'challenge',component:ChallengeComponent},
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
